bcf732936b9d3bc4d77a1daeb12e15b48c71f2b7
Nikita <busyrevnikita000@gmail.com>
2023-08-05 10:56:30 +0300

    add Phone Mask

--- scrtipts/main.js
+++ scrtipts/main.js
@@ -7,3 +7,17 @@ navBurger.onclick = function () {
     navIcon.classList.toggle('nav-icon--active');
     headerTopBlock.classList.toggle('header__top--mobile');
 }
+
+// phoneMask
+mask('[data-tel-input]');
+
+const phoneMask = document.querySelectorAll('[data-tel-input]');
+
+phoneMask.forEach(element => {
+    element.addEventListener('input', () => {
+        if (element.value == "+") element.value = "";
+    })
+    element.addEventListener('blur', () => {
+        if (element.value === "+") element.value = "";
+    })
+});
